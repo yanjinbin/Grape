59185e3565fae0df2feddf27d2b1b5e9c5b46f39
dabin <yanjinbin@qq.com>
2016-12-04 18:22:16 +0800

    ----增加手机校验规则

--- Section-1/src/main/java/CommonUtil/RegexUtil.java
+++ Section-1/src/main/java/CommonUtil/RegexUtil.java
@@ -1,5 +1,7 @@
 package CommonUtil;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +18,7 @@ public class RegexUtil {
 
     public static final String ip4 = "\\\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\b";
 
+    public static final String mobile_pattern = "^((17[0-9])|(18[0-9])|(14[0-9])|(13[0-9])|(15[0-9])|(18[0-9]))\\d{8}$";
 
     public static final String email = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 
@@ -30,27 +33,47 @@ public class RegexUtil {
 
     //18位身份证校验规则
     public static Boolean checkIdCard(String identityCard) {
-
+        if (StringUtils.isEmpty(identityCard)) {
+            return false;
+        }
         Pattern p = Pattern.compile(idCard);
         Matcher matcher = p.matcher(identityCard);
         return matcher.matches();
     }
 
     public static Boolean checkIP4(String ip) {
+        if (StringUtils.isEmpty(ip)) {
+            return false;
+        }
         Pattern p = Pattern.compile(ip4);
         Matcher m = p.matcher(ip);
         return m.matches();
     }
 
     public static Boolean checkEmail(String email) {
+        if (StringUtils.isEmpty(email)) {
+            return false;
+        }
         Pattern p = Pattern.compile(RegexUtil.email);
         Matcher m = p.matcher(email);
         return m.matches();
     }
 
     public static Boolean checkUserName(String userName){
+        if (StringUtils.isEmpty(userName)) {
+            return false;
+        }
         Pattern p = Pattern.compile(userName);
         Matcher m= p.matcher(userName);
+        return m.matches();
+    }
+
+    public static Boolean checkPhone(String phone){
+        if (StringUtils.isEmpty(phone)) {
+            return false;
+        }
+        Pattern p = Pattern.compile(mobile_pattern);
+        Matcher m = p.matcher(phone);
         return m.matches();
     }
 }
