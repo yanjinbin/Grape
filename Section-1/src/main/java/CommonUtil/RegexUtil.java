401c30eafb37bf6ce98a757bfc330df1e5fd524a
dabin <yanjinbin@qq.com>
2016-12-04 15:04:51 +0800

    --- 修改RegexUtil  正则表达式字符串 变成final static String类型filde name

--- Section-1/src/main/java/CommonUtil/RegexUtil.java
+++ Section-1/src/main/java/CommonUtil/RegexUtil.java
@@ -10,33 +10,47 @@ import java.util.regex.Pattern;
  */
 public class RegexUtil {
 
+    public static final String password = "^(?=.*\\d).(?=.*[a-z]).(?=.*[A-Z]).{8,10}$";
+
+    public static final String idCard = "^[1-9]\\\\d{5}[1-9]\\\\d{3}((0\\\\d)|(1[0-2]))(([0|1|2]\\\\d)|3[0-1])\\\\d{3}([0-9]|X)$";
+
+    public static final String ip4 = "\\\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\b";
+
+
+    public static final String email = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+
+    public static final String userName =""; //nextdo
+
     //校验密码强度，规则：包含大小写字母和数字的组合，不能使用特殊字符，长度在8-10之间
-    public static Boolean isPasswordValid(String password) {
-        String passwordPattern = "^(?=.*\\d).(?=.*[a-z]).(?=.*[A-Z]).{8,10}$";
-        Pattern p = Pattern.compile(passwordPattern);
+    public static Boolean checkPassword(String password) {
+        Pattern p = Pattern.compile(RegexUtil.password);
         Matcher m = p.matcher(password);
         return m.matches();
     }
 
     //18位身份证校验规则
-    public static Boolean isIdCardValid(String identityCard) {
-        String idCardPattern = "^[1-9]\\\\d{5}[1-9]\\\\d{3}((0\\\\d)|(1[0-2]))(([0|1|2]\\\\d)|3[0-1])\\\\d{3}([0-9]|X)$";
-        Pattern p = Pattern.compile(idCardPattern);
+    public static Boolean checkIdCard(String identityCard) {
+
+        Pattern p = Pattern.compile(idCard);
         Matcher matcher = p.matcher(identityCard);
         return matcher.matches();
     }
 
-    public static Boolean isIP4Valid(String ip) {
-        String ip4pattern = "\\\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\\\b";
-        Pattern p = Pattern.compile(ip4pattern);
+    public static Boolean checkIP4(String ip) {
+        Pattern p = Pattern.compile(ip4);
         Matcher m = p.matcher(ip);
         return m.matches();
     }
 
-    public static Boolean isEmailInvalid(String email) {
-        String emailPattern = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-        Pattern p = Pattern.compile(emailPattern);
+    public static Boolean checkEmail(String email) {
+        Pattern p = Pattern.compile(RegexUtil.email);
         Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    public static Boolean checkUserName(String userName){
+        Pattern p = Pattern.compile(userName);
+        Matcher m= p.matcher(userName);
         return m.matches();
     }
 }
