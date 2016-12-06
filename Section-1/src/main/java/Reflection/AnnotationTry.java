ab7095d0457c763363ea9ffcdb6a3780f4a5eebe
dabin <yanjinbin@qq.com>
2016-12-06 11:44:24 +0800

    ----Annotation实践

---
+++ Section-1/src/main/java/Reflection/AnnotationTry.java
@@ -0,0 +1,11 @@
+package Reflection;
+
+/**
+ * @author Silver & Bullet
+ *         since 2016-十二月
+ */
+public class AnnotationTry {
+    public static void main(String[] args) {
+        System.out.println("--------进入annotation实践--------");
+    }
+}
