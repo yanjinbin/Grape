d3800594fe3efc283c3e8e33d776aa7e027785d7
dabin <yanjinbin@qq.com>
2016-12-06 13:47:06 +0800

    ---java 反射获取注解   https://goo.gl/LVoZKq  https://goo.gl/EiKzce

--- Section-1/src/main/java/Reflection/AnnotationTry.java
+++ Section-1/src/main/java/Reflection/AnnotationTry.java
@@ -1,5 +1,10 @@
 package Reflection;
 
+import Annotations.Preamble;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 /**
  * @author Silver & Bullet
  *         since 2016-十二月
@@ -7,5 +12,35 @@ package Reflection;
 public class AnnotationTry {
     public static void main(String[] args) {
         System.out.println("--------进入annotation实践--------");
+        Annotation[] annotations = Parent.class.getAnnotations();
+        Method[] methods = Parent.class.getMethods();
+        for (Method item :
+                methods) {
+            Annotation[][] parameterAnnotations = item.getParameterAnnotations();
+            for (Annotation[] annotationArray :
+                    parameterAnnotations) {
+                for (Annotation annotation :annotationArray
+                     ) {
+                    if (annotation instanceof SuppressWarnings) {
+                        System.out.println("value值-----"+((SuppressWarnings) annotation).value());
+                    }
+                }
+            }
+
+        }
+
+        for (Annotation item :
+                annotations) {
+            System.out.println(item.getClass().getName());
+            if (item instanceof Preamble) {
+                Preamble preamble = (Preamble) item;
+                String[] names = preamble.names();
+                for (String s :
+                        names) {
+                    System.out.println("-------"+s);
+                }
+                System.out.println("author"+preamble.author());
+            }
+        }
     }
 }
