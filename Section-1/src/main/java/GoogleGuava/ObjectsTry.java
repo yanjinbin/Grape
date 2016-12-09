0c6c40da0062e5747beaf974df681e466baba63a
Silver & Bullet <yanjinbin@gmail.com>
2016-12-09 16:22:55 +0800

    Guava Objects 重写 fluent flavor

---
+++ Section-1/src/main/java/GoogleGuava/ObjectsTry.java
@@ -0,0 +1,29 @@
+package GoogleGuava;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+/**
+ * @author Silver & Bullet
+ *         since 2016-十二月
+ */
+public class ObjectsTry {
+    public static void main(String[] args) {
+        boolean equal = Objects.equal(i, j);
+        System.out.println("is equal ?   " + equal);
+        boolean newEqual = Objects.equal(m, n);
+        System.out.println("is equal ?   " + newEqual);
+        //输出哈希值
+        System.out.println("输出hashcode     " + Objects.hashCode(i, j, m, n));
+        //重写toString方法 beautify code
+        MoreObjects.ToStringHelper string = MoreObjects.toStringHelper(AvoidNullTry.class).add("I", 3).add("J", 4);
+        System.out.println(string);
+
+    }
+
+    public static int i = 3;
+    private static int j = 3;
+
+    public static Integer m = new Integer(3);
+    public static Integer n = new Integer(3);
+}
