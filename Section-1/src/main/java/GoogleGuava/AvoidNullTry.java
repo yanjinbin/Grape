b83ac4ccbf2ed55d3a8768b3622981fb0fb26794
Silver & Bullet <yanjinbin@gmail.com>
2016-12-09 16:26:03 +0800

    Guava null的语义是absent的意思

--- Section-1/src/main/java/GoogleGuava/AvoidNullTry.java
+++ Section-1/src/main/java/GoogleGuava/AvoidNullTry.java
@@ -1,13 +1,16 @@
 package GoogleGuava;
 
 
-import com.google.api.client.repackaged.com.google.common.base.Optional;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Optional;
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
- * null的处理
+ * null的处理 absent 和present 2种语义
  *
  * @author Silver & Bullet
  *         since 2016-十二月
@@ -19,15 +22,36 @@ public class AvoidNullTry {
         System.out.println("is Present " + stringOptional.isPresent());
         //     Optional<String> mOption = Optional.of(m);//快速抛出null pointer exception异常
         Optional<String[]> stringOption = Optional.of(stringArray);
-        String[] strings = stringOption.get();
-        Set<String[]> set = stringOption.asSet();
-        Iterator<String[]> iterator = set.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.toString());
-        }
+        Set<String[]> stringSet = stringOption.asSet();
+        Optional<String> mOptional = Optional.fromNullable(m);
+        boolean equals = Optional.absent().equals(mOptional);
+//        arrayList.add("1");
+//        arrayList.add("2");
+//        arrayList.add(null);
+//        Iterable iterable = Optional.presentInstances(arrayList);
+//        Iterator iterator = iterable.iterator();
+//        while (iterator.hasNext()) {
+//            System.out.println(iterator.next());
+//        }
+        System.out.println("is Present ?  " + mOptional.isPresent());
+        String defaultStr = mOptional.or("默认值1");
+        System.out.println(defaultStr);
 
+        String s = MoreObjects.firstNonNull(m, AvoidNullTry.s);//取不为null的值,取不到 抛null pointer
+        System.out.println("返回 s的值   " + s.equals(s));
+
+        String s1 = Optional.of(emptyString).orNull();
+        System.out.println("s1 is null?  " + StringUtils.isEmpty(s1));
+
+        String s2 = Optional.fromNullable(m).orNull();
+        System.out.println("s2 is null?  " + StringUtils.isEmpty(s2));
+        System.out.println("s2 is null or empty " + Strings.isNullOrEmpty(s2));
+        System.out.println("s2 is empty -----" + Strings.nullToEmpty(s2) + "----");
+        //   stringOption.transform()
 
     }
+
+    public static ArrayList arrayList = new ArrayList();
 
     public static String s = "s";
 
@@ -35,8 +59,9 @@ public class AvoidNullTry {
 
     public static String m = null;
 
-    public static String[] stringArray = {"a", null, "n", "b"};
+    public static String[] stringArray = {"a", "b", "n", "b"};
 
     public static int[] intArray = {1, 2, 3, 4, 5};
+
 
 }
