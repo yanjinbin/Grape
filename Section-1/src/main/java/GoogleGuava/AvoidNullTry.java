832986d0fd83a4e72be7e433e39e0d251c48f139
Silver & Bullet <yanjinbin@gmail.com>
2016-12-09 14:41:17 +0800

    -----重新定义maven工程

--- Section-1/src/main/java/GoogleGuava/AvoidNullTry.java
+++ Section-1/src/main/java/GoogleGuava/AvoidNullTry.java
@@ -3,6 +3,9 @@ package GoogleGuava;
 
 import com.google.api.client.repackaged.com.google.common.base.Optional;
 
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * null的处理
  *
@@ -12,33 +15,28 @@ import com.google.api.client.repackaged.com.google.common.base.Optional;
 public class AvoidNullTry {
     public static void main(String[] args) {
         System.out.println("---guava实战--");
-        Optional<Integer> possible = Optional.of(5);
-        possible.isPresent();
-        Optional<Object> absent1 = possible.absent();
-        
-        Integer integer = possible.get();
-        Optional<Object> absent = absent1;
-        Optional<String[]> fromNullable = Optional.fromNullable(array);//turn the given possible-null reference into an Optional
-        //   System.out.println(fromNullable.isPresent());
-//        String[] or = fromNullable.or(array);
-//        for (String item :
-//                or) {
-//            System.out.println(item);
-//        }
-//        String[] strings = fromNullable.get();
-//        System.out.println(fromNullable.isPresent());
-//        Optional<Object> optional = Optional.of(null);//如果是null  throw NullPointerException
-//        optional.isPresent();
-//        optional.get();
+        Optional<String> stringOptional = Optional.fromNullable(s); // s m
+        System.out.println("is Present " + stringOptional.isPresent());
+        //     Optional<String> mOption = Optional.of(m);//快速抛出null pointer exception异常
+        Optional<String[]> stringOption = Optional.of(stringArray);
+        String[] strings = stringOption.get();
+        Set<String[]> set = stringOption.asSet();
+        Iterator<String[]> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.toString());
+        }
 
-        Optional optional = getOptional(array);
-        System.out.println("不包含Null引用?------" + optional.isPresent());
+
     }
 
-    public static String[] array = {"a", null, "n", "b"};
+    public static String s = "s";
 
-    public static Optional getOptional(String[] array) {
-        //  return Optional.fromNullable(array);
-        return Optional.of(array);
-    }
+    public static String emptyString = "";
+
+    public static String m = null;
+
+    public static String[] stringArray = {"a", null, "n", "b"};
+
+    public static int[] intArray = {1, 2, 3, 4, 5};
+
 }
