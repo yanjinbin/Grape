c870af3f8e55e3083325151acb003e4844ead9f6
yanjinbin <yanjinbin@qq.com>
2016-12-10 22:23:55 +0800

    ---对throwable的进一步理解

--- Section-1/src/main/java/Foundation/ExceptionTry.java
+++ Section-1/src/main/java/Foundation/ExceptionTry.java
@@ -1,7 +1,5 @@
 package Foundation;
 
-import java.sql.SQLException;
-
 /**
  * https://goo.gl/3Edt0L
  *
@@ -11,19 +9,24 @@ public class ExceptionTry {
     public static void main(String[] args) {
         //unchecked exception ,属于RuntimeException的之类,RuntimeException和Error,是unchecked exception 两大类,
         //RuntimeException 一般是程序逻辑错误,可以不处理,但是原则上都应该去思考尽可能避免发生 defensive programming ,如果碰到了,那么应当需要try catch住
-        throwNullExce("哈哈");
+
+        throwNullEXEC("哈哈");
+
+
         try {//checked exception 编译时候就会不通过,必须程序自己处理好
             throwSQLEXEC("嘿嘿");
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void throwNullExce(String s)throws NullPointerException{
+    public static void throwNullEXEC(String s) throws NullPointerException {
         System.out.println("s的值   "+s);
     }
 
-    public static void throwSQLEXEC(String s) throws SQLException{
-        System.out.println("s value is");
+    public static void throwSQLEXEC(String s) throws Exception {
+        System.out.println("s value is  " + s);
+        throw new IllegalArgumentException("hah");
+        //  throw new IOException("aauwuue");
     }
 }
