a1791039462a588d6cbe6e96f0fb50bc0a2317e9
Silver Bullet <yanjinbin@gmail.com>
2016-12-11 11:52:56 +0800

    MultiMap实践

---
+++ Section-1/src/main/java/GoogleGuava/GuavaCollection/MultiMapTry.java
@@ -0,0 +1,102 @@
+package GoogleGuava.GuavaCollection;
+
+import com.google.common.base.Objects;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multiset;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * MultiMap处理key到多个values也就是collection的映射关系
+ * @author Silver & Bullet
+ *         on 十二月-11
+ */
+public class MultiMapTry {
+    public static void main(String[] args) {
+        ImmutableMultimap<Integer, String> immutableMultimap = ImmutableMultimap.of(1, "a").of(2, "b").of(3, "c");
+        //?
+        boolean isExist = immutableMultimap.containsValue("b");
+        boolean containsKey = immutableMultimap.containsKey(1);
+        System.out.println("is exist ?   "+isExist);
+        System.out.println("contains key  ?"+containsKey);
+
+
+        HashMultimap<Object, Object> hashMultimap = HashMultimap.create();
+        hashMultimap.put(1,"e");
+        hashMultimap.put(2,"q");
+        hashMultimap.put(3,"z");
+        hashMultimap.put(3,"y");
+        boolean isContainsEntry = hashMultimap.containsEntry(2, "z");
+
+        System.out.println("contains key-value pair ?   "+isContainsEntry);
+        Multiset<Object> keys = hashMultimap.keys();
+
+        System.out.println("size is ?   "+keys.size());
+
+        Set<Map.Entry<Object, Object>> entries = hashMultimap.entries();
+
+        System.out.println("entry size is ?  "+entries.size());
+
+
+        Map<Object, Collection<Object>> collectionMap = hashMultimap.asMap();
+        boolean containsValue = hashMultimap.containsValue("e");
+        System.out.println("contain value ?  "+containsValue);
+        Multimap multimap=hashMultimap;
+        boolean b = hashMultimap.putAll(hashMultimap);
+        System.out.println("two objects is equal ?  "+Objects.equal(multimap,hashMultimap));
+
+
+        System.out.println("---------分割线在此-------");
+
+        Iterator<Map.Entry<Object, Object>> entryIterator = hashMultimap.entries().iterator();
+        while (entryIterator.hasNext()){
+            System.out.println(entryIterator.next().toString());
+        }
+
+        Iterator<Object> keyIterator = hashMultimap.keys().iterator();
+        while (keyIterator.hasNext()){
+            System.out.println(keyIterator.next());
+        }
+
+        Iterator<Object> valueIterator = hashMultimap.values().iterator();
+        while (valueIterator.hasNext()){
+            System.out.println(valueIterator.next());
+        }
+
+        System.out.println("---------分割线在此-------");
+
+        TreeSet<String> strings = new TreeSet<String>();
+        strings.add("w");
+        strings.add("p");
+        Collection collection2 = multimap.get(2);
+        Iterator iterator = collection2.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("---------分割线在此-------");
+
+
+        Collection collection1 = multimap.replaceValues(2, strings);
+        System.out.println(collection1.size());
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("two collection is equal ?  "+Objects.equal(collection1,collection2));
+        boolean contains = multimap.removeAll(3).contains(3);
+        System.out.println("is still contain 3 ?   "+contains);
+
+
+        multimap.clear();
+        System.out.println("is Empty ?   "+multimap.isEmpty());
+        //  ImmutableMultimap.copyOf()
+
+
+    }
+}
