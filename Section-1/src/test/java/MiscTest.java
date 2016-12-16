3b96217b23735a520c325fc78f72d88863d4f0b0
Silver Bullet <yanjinbin@gmail.com>
2016-12-16 21:05:53 +0800

    ----CollectionUtil使用方法

--- Section-1/src/test/java/MiscTest.java
+++ Section-1/src/test/java/MiscTest.java
@@ -1,3 +1,4 @@
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
@@ -10,7 +11,7 @@ import java.util.List;
  *         on 十二月-16
  */
 
-public class MiscTest {
+public class MiscTest  extends BaseTest{
 
     @Test
     public void emptyBlankTest(){
@@ -48,6 +49,22 @@ public class MiscTest {
         //Iterable有一个Iterator iterator() 方法
 
         //Collection接口 extends Iterable 接口 所以所有的 集合都实现了迭代的功能
+
+    }
+    @Test
+    public void CollectionUtilTest(){
+
+        List arrayList = new ArrayList();
+
+        System.out.println(CollectionUtils.isEmpty(arrayList));
+
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+
+        System.out.println(CollectionUtils.isEmpty(arrayList));
+
 
     }
 }
