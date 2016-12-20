21fbe1cb7f3b032f4530e20714905ae6fc73ee25
Silver & Bullet <yanjinbin@gmail.com>
2016-12-20 19:24:00 +0800

    3 kinds Iterator In Action

---
+++ Section-1/src/main/java/GoogleGuava/OverPeekingIterator/PeekingIteratorSkipDuplicateValue.java
@@ -0,0 +1,26 @@
+package GoogleGuava.OverPeekingIterator;
+
+import com.google.common.collect.Iterators;
+import com.google.common.collect.PeekingIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * @author Silver Bullet
+ * @since 十二月-20
+ */
+public class PeekingIteratorSkipDuplicateValue extends ArrayList{
+    @Override
+    public Iterator iterator() {
+        PeekingIterator peekingIterator = Iterators.peekingIterator(super.iterator());
+        while (peekingIterator.hasNext()){
+            Object next = peekingIterator.next();
+            while(peekingIterator.hasNext()&&peekingIterator.peek().equals(next)){
+                peekingIterator.next();
+            }
+            add(next);
+        }
+        return peekingIterator;
+    }
+}
