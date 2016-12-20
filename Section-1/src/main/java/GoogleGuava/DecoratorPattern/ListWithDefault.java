0f6f361cac332dc28386fbffafecc7014d2506bf
Silver & Bullet <yanjinbin@gmail.com>
2016-12-20 18:58:09 +0800

    ---- Guava Decorator 实战

--- Section-1/src/main/java/GoogleGuava/DecoratorPattern/ListWithDefault.java
+++ Section-1/src/main/java/GoogleGuava/DecoratorPattern/ListWithDefault.java
@@ -7,18 +7,21 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
+ *
+ * Guava 装饰者 模式  这是建立在你对JDK Collection 相当熟悉的情况下,知道JDK的优缺点,和了解装饰者模式的情况下,才能对JDK Collection 集合框架 做出符合业务需求的改动
  * @author Silver Bullet
  * @since 十二月-20
  */
 public class ListWithDefault<E> extends ForwardingList<E> {
-    final  E defaultValue;
+    final E defaultValue;
 
-    final  List<E> delegate;
+    final List<E> delegate;
 
     protected ListWithDefault(E defaultValue, List<E> delegate) {
         this.defaultValue = defaultValue;
         this.delegate = delegate;
     }
+
     //must implement
     @Override
     protected List<E> delegate() {
@@ -28,22 +31,22 @@ public class ListWithDefault<E> extends ForwardingList<E> {
     @Override
     public E get(int index) {
         E v = super.get(index);
-        return  (v==null?defaultValue:v);
+        return (v == null ? defaultValue : v);
     }
 
     @Override
     public Iterator<E> iterator() {
-        final  Iterator<E> iter = super.iterator();
-        return  new ForwardingIterator<E>() {
+        final Iterator<E> iter = super.iterator();
+        return new ForwardingIterator<E>() {
             @Override
             protected Iterator<E> delegate() {
-                return  iter;
+                return iter;
             }
 
             @Override
             public E next() {
                 E v = super.next();
-                return (v==null?defaultValue:v);
+                return (v == null ? defaultValue : v);
             }
         };
     }
