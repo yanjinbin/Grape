bd4b1b54a4f4c88c062bd20d3396d40da74a7b7b
Silver & Bullet <yanjinbin@gmail.com>
2016-12-20 19:05:37 +0800

    List With Default Value

--- Section-1/src/main/java/GoogleGuava/DecoratorPattern/ListWithDefaultValue.java
+++ Section-1/src/main/java/GoogleGuava/DecoratorPattern/ListWithDefaultValue.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- *
+ * 参考这篇文章  http://stackoverflow.com/questions/3502066/guava-forwardinglist-usage-example/#_=_
  * Guava 装饰者 模式  这是建立在你对JDK Collection 相当熟悉的情况下,知道JDK的优缺点,和了解装饰者模式的情况下,才能对JDK Collection 集合框架 做出符合业务需求的改动
  * @author Silver Bullet
  * @since 十二月-20
@@ -30,7 +30,7 @@ public class ListWithDefaultValue<E> extends ForwardingList<E> {
 
     @Override
     public E get(int index) {
-        //return super.get(index);  不然的话,会和逾期不一致
+        //return super.get(index);  不然的话,会和预期不一致
         E v = super.get(index);
         return (v == null ? defaultValue : v);
     }
