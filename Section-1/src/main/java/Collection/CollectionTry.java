8dae27cba338e7c26398ad8b45cd962963373157
Silver & Bullet <yanjinbin@gmail.com>
2017-01-05 19:32:16 +0800

    ---jdk collection

---
+++ Section-1/src/main/java/Collection/CollectionTry.java
@@ -0,0 +1,11 @@
+package Collection;
+
+/**
+ * @author Silver Bullet
+ * @since 一月-05
+ */
+public class CollectionTry {
+    public static void main(String[] args) {
+        // java  原生的集合框架  暂时放弃 , 已经比较熟悉了 不用再看了
+    }
+}
