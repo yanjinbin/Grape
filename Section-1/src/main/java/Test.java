ddfbe7ea4f8f7a2f4014cf9835e6df218c558f7a
Silver & Bullet <yanjinbin@gmail.com>
2017-01-13 17:55:47 +0800

    ---IDE serialVersionUID 生成设置

---
+++ Section-1/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+/**
+ * @author Silver Bullet
+ * @since 一月-10
+ */
+public class Test implements Serializable {
+
+    private static final long serialVersionUID = 2010307013874058143L;
+}
