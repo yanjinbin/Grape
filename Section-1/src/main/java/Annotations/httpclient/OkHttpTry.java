ab0b3ef007b8bb46c1b78ed97f51f50643fbcd42
Silver & Bullet <yanjinbin@gmail.com>
2017-02-24 10:19:05 +0800

    okhttp client and String.format() function

--- Section-1/src/main/java/Annotations/httpclient/OkHttpTry.java
+++ Section-1/src/main/java/Annotations/httpclient/OkHttpTry.java
@@ -1,30 +1,39 @@
 package Annotations.httpclient;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import com.google.common.net.MediaType;
 
 import java.io.IOException;
 
 /**
+ * http://dwz.cn/5mqkTw
+ * <p>
+ * http://dwz.cn/5mqmYF
+ * <p>
+ * http://dwz.cn/5mqtUF
+ * okhttp client使用
+ *
  * @author Silver & Bullet
  *         on 二月-21
  */
 public class OkHttpTry {
     public static void main(String[] args) throws IOException {
 
-        OkHttpClient okHttpClient = new OkHttpClient();
-        String url = null;
-        Request build = new Request.Builder().url(url).build();
-        Response response = okHttpClient.newCall(build).execute();
-        String string = response.body().string();
-
-String json =null;
-        RequestBody requestBody = RequestBody.create(MediaType.parse("text/html"), json);
-
-        Request post = new Request.Builder().url(url).post(requestBody).build();
-        String postString = okHttpClient.newCall(post).execute().body().string();
+//
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        String url = null;
+//        Request build = new Request.Builder().url(url).build();
+//        Response response = okHttpClient.newCall(build).execute();
+//        String string = response.body().string();
+//
+//String json =null;
+//        RequestBody requestBody = RequestBody.create(MediaType.parse("text/html"), json);
+//
+//        Request post = new Request.Builder().url(url).post(requestBody).build();
+//        String postString = okHttpClient.newCall(post).execute().body().string();
+//        String userAgent = HTTP.USER_AGENT;
+//        String connClose = HTTP.CONN_CLOSE;
+        String s = MediaType.JSON_UTF_8.toString();
+        String type = s;
+        System.out.println(type);
     }
 }
