d6be5ead02dd0355bd49d093dd9e50da9dce88f5
Silver & Bullet <yanjinbin@gmail.com>
2017-03-03 18:55:53 +0800

    ----UserController  构建restful api

--- springboot/src/main/java/BaseApplication.java
+++ springboot/src/main/java/BaseApplication.java
@@ -1,11 +1,13 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Silver Bullet
  * @since 三月-03
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "springboot")
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class);
