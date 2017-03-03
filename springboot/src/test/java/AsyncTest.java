b8cff423aa68030d16d845c02b4f51a6bc5d3e43
Silver Bullet <yanjinbin@gmail.com>
2017-03-04 00:19:26 +0800

    异步调用

---
+++ springboot/src/test/java/AsyncTest.java
@@ -0,0 +1,28 @@
+import springboot.MultiTask;
+
+import java.util.concurrent.Future;
+
+/**
+ * @author Silver & Bullet
+ *         on 三月-04
+ */
+public class AsyncTest extends BaseTest{
+
+    public void asyncTest() throws InterruptedException {
+        long start=System.currentTimeMillis();
+        MultiTask task = new MultiTask();
+        Future<String> taskOne = task.doTaskOne();
+        Future<String> taskTwo = task.doTaskTwo();
+        Future<String> taskThree = task.doTaskThree();
+        while(true) {
+            if(taskOne.isDone() && taskTwo.isDone() && taskThree.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+
+    }
+}
