848cb79c2d3c5119bab5996401a7afaebb488e43
Silver & Bullet <yanjinbin@gmail.com>
2017-03-03 19:36:07 +0800

    ----Redis

---
+++ springboot/src/main/java/springboot/RedisUtil/RedisKey.java
@@ -0,0 +1,23 @@
+package springboot.RedisUtil;
+
+
+import lombok.Getter;
+
+/**
+ * @author Silver Bullet
+ * @since 十二月-28
+ */
+
+public enum RedisKey {
+
+    ;
+    @Getter
+    private String prefix;
+    @Getter
+    private Integer expiredTime;
+
+    RedisKey(String prefix, Integer expiredTime) {
+        this.prefix = prefix;
+        this.expiredTime = expiredTime;
+    }
+}
