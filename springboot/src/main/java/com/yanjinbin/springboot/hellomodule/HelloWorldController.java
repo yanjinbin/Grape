a8553f3dff51e20d874711d79215fbaeaee9751c
Silver & Bullet <yanjinbin@gmail.com>
2017-03-10 20:07:15 +0800

    response set http key value is invalidate

--- springboot/src/main/java/com/yanjinbin/springboot/hellomodule/HelloWorldController.java
+++ springboot/src/main/java/com/yanjinbin/springboot/hellomodule/HelloWorldController.java
@@ -1,18 +1,27 @@
 package com.yanjinbin.springboot.hellomodule;
 
 import com.yanjinbin.springboot.Util.ViewResult;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author Silver Bullet
  * @since 三月-03
  */
 @RestController
+@RequestMapping("/hello/")
 public class HelloWorldController {
 
-    @RequestMapping(value = "/hello")
-    public ViewResult hello(String a) {
+    @RequestMapping(value = "world")
+    public ViewResult hello(HttpServletRequest request, HttpServletResponse response,String a) throws IOException {
+        response.setHeader("x-xcrf-token","1111");
+        response.sendRedirect("localhost:8080/user/get/1");
+        response.setStatus(HttpStatus.OK.value());
         return ViewResult.createSuccess(a, "你大爷");
     }
 }
