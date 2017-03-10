fccfac71eb17b445eec06d5997eab74b7f5227bf
Silver & Bullet <yanjinbin@gmail.com>
2017-03-10 20:05:27 +0800

    IF without eles if

---
+++ Section-1/src/main/java/CommonUtil/IFELSETry.java
@@ -0,0 +1,25 @@
+package CommonUtil;
+
+/**
+ * @author Silver Bullet
+ * @since 三月-10
+ */
+public class IFELSETry {
+    public static void main(String[] args) {
+        System.out.println(trick(11, 1));
+
+
+    }
+
+    public static Boolean trick(Integer i, Integer j) {
+        if (i == 10 && j == 1) {
+            return false;
+        }
+        /* else */
+        if (i == 10 && j != 1) {
+            return true;
+        }
+        return false;
+
+    }
+}
