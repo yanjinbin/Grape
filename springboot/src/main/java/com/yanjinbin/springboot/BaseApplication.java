a08f4c3af16cb4c8de79b301592523a5809c155b
Silver & Bullet <yanjinbin@gmail.com>
2017-03-10 20:06:39 +0800

    添加  拦截器

--- springboot/src/main/java/com/yanjinbin/springboot/BaseApplication.java
+++ springboot/src/main/java/com/yanjinbin/springboot/BaseApplication.java
@@ -3,15 +3,22 @@ package com.yanjinbin.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Silver Bullet
  * @since 三月-03
  */
 @SpringBootApplication
-@ComponentScan(basePackages = "springboot")
-public class BaseApplication {
+@ComponentScan(basePackages = "com.yanjinbin.springboot")
+public class BaseApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GeneralInterceptor()).addPathPatterns("/user/");
     }
 }
