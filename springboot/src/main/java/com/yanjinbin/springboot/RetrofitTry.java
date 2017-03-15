24c5915ba2d0f0ce132afe734f008431fdc12ab8
Silver Bullet <yanjinbin@gmail.com>
2017-03-15 18:31:40 +0800

    http 2 interface try

---
+++ springboot/src/main/java/com/yanjinbin/springboot/RetrofitTry.java
@@ -0,0 +1,12 @@
+package com.yanjinbin.springboot;
+
+/**
+ * @author Silver & Bullet
+ *         on 三月-15
+ *
+ *         http://square.github.io/retrofit/
+ *
+ * 讲HTTP调用 编程接口形式
+ */
+public class RetrofitTry {
+}
