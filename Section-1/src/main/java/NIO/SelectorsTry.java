73d477093d033e167a6b1e076aa1821d069c1a3b
Silver Bullet <yanjinbin@gmail.com>
2017-03-20 16:33:02 +0800

    selector on thread

---
+++ Section-1/src/main/java/NIO/SelectorsTry.java
@@ -0,0 +1,16 @@
+package NIO;
+
+/**
+ * http://ifeve.com/selectors/
+ * 选择器,一个线程可以监听多个NIO 通道,并知晓是否为读写做好准备的组件
+ *
+ * 为什么要使用单个线程,因为线程之间切换上下文,系统开销比较大
+ *
+ * @author Silver & Bullet
+ *         on 三月-20
+ */
+public class SelectorsTry {
+    public static void main(String[] args) {
+
+    }
+}
