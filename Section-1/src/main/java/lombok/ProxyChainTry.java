ce0e5aab261a1e44d40e21030bf3039914fb8b9e
Silver Bullet <yanjinbin@gmail.com>
2017-03-21 15:47:56 +0800

    lombok @Delegate 的使用

--- Section-1/src/main/java/lombok/ProxyChainTry.java
+++ Section-1/src/main/java/lombok/ProxyChainTry.java
@@ -1,11 +1,14 @@
 package lombok;
 
 /**
+ *
+ * https://goo.gl/s7avNa
+ *
  * @author Silver & Bullet
  *         on 三月-21
  */
 public class ProxyChainTry {
     public static void main(String[] args) {
-
+        //主要涉及 lombook @delegate 的使用
     }
 }
