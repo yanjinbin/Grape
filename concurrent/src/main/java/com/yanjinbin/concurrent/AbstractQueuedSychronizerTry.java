9a93eeb13c0faf89f404a34cf379464984e07555
yanjinbin <yanjinbin@gmail.com>
2017-05-09 23:31:33 +0800

    AQS AQS Node源码

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/AbstractQueuedSychronizerTry.java
@@ -0,0 +1,16 @@
+package com.yanjinbin.concurrent;
+
+/**
+ * http://www.infoq.com/cn/articles/jdk1.8-abstractqueuedsynchronizer
+ * http://www.infoq.com/cn/articles/java8-abstractqueuedsynchronizer
+ * https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/AbstractQueuedSynchronizer.html
+ * http://ifeve.com/introduce-abstractqueuedsynchronizer/
+ * http://www.cnblogs.com/zhanjindong/p/java-concurrent-package-aqs-AbstractQueuedSynchronizer.html
+ * http://www.cnblogs.com/go2sea/p/5618628.html
+ * http://www.cs.rochester.edu/wcms/research/systems/high_performance_synch/
+ *
+ * @Author Silver bullet
+ * @Since 2017/5/9.
+ */
+public class AbstractQueuedSychronizerTry {
+}
