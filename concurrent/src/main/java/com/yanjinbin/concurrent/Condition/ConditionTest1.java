f392773f24ac2ad3ef60665c20651d90e303f1e7
yanjinbin <yanjinbin@gmail.com>
2017-05-15 20:34:03 +0800

    Conidition 用法举例

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/Condition/ConditionTest1.java
@@ -0,0 +1,56 @@
+package com.yanjinbin.concurrent.Condition;
+
+/**
+ * Condition高级用法
+ * http://www.cnblogs.com/skywang12345/p/3496716.html
+ *
+ * @Author Silver bullet
+ * @Since 2017/5/15.
+ */
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionTest1 {
+
+    private static Lock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+
+    public static void main(String[] args) {
+
+        ThreadA ta = new ThreadA("ta");
+
+        lock.lock(); // 获取锁
+        try {
+            System.out.println(Thread.currentThread().getName() + " start ta");
+            ta.start();
+
+            System.out.println(Thread.currentThread().getName() + " block");
+            condition.await();    // 等待
+
+            System.out.println(Thread.currentThread().getName() + " continue");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();    // 释放锁
+        }
+    }
+
+    static class ThreadA extends Thread {
+
+        public ThreadA(String name) {
+            super(name);
+        }
+
+        public void run() {
+            lock.lock();    // 获取锁
+            try {
+                System.out.println(Thread.currentThread().getName() + " wakup others");
+                condition.signal();    // 唤醒“condition所在锁上的其它线程”
+            } finally {
+                lock.unlock();    // 释放锁
+            }
+        }
+    }
+}
