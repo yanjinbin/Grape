7e04263d7c341b8ed830d508504832a67e5d649c
yanjinbin <yanjinbin@gmail.com>
2017-05-15 20:36:16 +0800

    ---- nothing

--- concurrent/src/main/java/Misc.java
+++ concurrent/src/main/java/Misc.java
@@ -7,6 +7,25 @@ public class Misc {
         if (true && false) {
             System.out.println("哈哈哈");
         }
+        int arg = 1;
+        if (!tryAcquire(arg) && acquireQueue(arg)) {
+            System.out.println("success !!");
+        }
     }
 
+
+    public static boolean tryAcquire(int arg) {
+        if (arg > 10) return true;
+        else return false;
+    }
+
+    public static boolean acquireQueue(int arg) {
+        if (arg < 15 && tryAcquire(arg)) {
+            arg++;
+            if (arg > 10) {
+                return tryAcquire(arg);
+            }
+        }
+        return false;
+    }
 }
