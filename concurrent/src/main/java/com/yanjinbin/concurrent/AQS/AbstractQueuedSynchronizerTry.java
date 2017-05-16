59a9907f6ea842e070236385acc397b96c070e19
yanjinbin <yanjinbin@gmail.com>
2017-05-16 14:52:37 +0800

    AQS

--- concurrent/src/main/java/com/yanjinbin/concurrent/AQS/AbstractQueuedSynchronizerTry.java
+++ concurrent/src/main/java/com/yanjinbin/concurrent/AQS/AbstractQueuedSynchronizerTry.java
@@ -18,7 +18,6 @@ import java.util.concurrent.locks.Condition;
  * http://www.infoq.com/cn/articles/jdk1.8-abstractqueuedsynchronizer
  * http://www.infoq.com/cn/articles/java8-abstractqueuedsynchronizer
  * https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/AbstractQueuedSynchronizer.html
- * http://ifeve.com/introduce-abstractqueuedsynchronizer/
  * http://www.cnblogs.com/zhanjindong/p/java-concurrent-package-aqs-AbstractQueuedSynchronizer.html
  * http://www.cnblogs.com/go2sea/p/5618628.html
  * http://www.cs.rochester.edu/wcms/research/systems/high_performance_synch/
