5f8a30012a6ef6f9ec4c7588b3edec0868d8246c
yanjinbin <yanjinbin@gmail.com>
2017-05-16 15:39:59 +0800

    java 线程池 学习

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/excutor/ExcutorTry.java
@@ -0,0 +1,10 @@
+package com.yanjinbin.concurrent.excutor;
+
+/**
+ * todo
+ *
+ * @Author Silver bullet
+ * @Since 2017/5/16.
+ */
+public class ExcutorTry {
+}
--- concurrent/src/main/java/com/yanjinbin/concurrent/excutor/Readme.md
+++ concurrent/src/main/java/com/yanjinbin/concurrent/excutor/Readme.md
@@ -1,2 +1,6 @@
 ---
 线程池
+
+java 线程池管理
+
+http://www.cnblogs.com/skywang12345/p/3509903.html
