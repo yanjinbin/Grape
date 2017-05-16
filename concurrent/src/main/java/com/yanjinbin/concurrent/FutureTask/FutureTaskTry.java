0117849524c1c21139236a04ecf84e1ffc75d43b
yanjinbin <yanjinbin@gmail.com>
2017-05-16 18:51:19 +0800

    ---todo Future

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/FutureTask/FutureTaskTry.java
@@ -0,0 +1,13 @@
+package com.yanjinbin.concurrent.FutureTask;
+
+/**
+ * todo  和guava 的 ListenableFuture 和 Service 一同学习
+ *
+ * @Author Silver bullet
+ * @Since 2017/5/16.
+ */
+public class FutureTaskTry {
+    public static void main(String[] args) {
+
+    }
+}
