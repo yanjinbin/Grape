1c00627e171a4b9900dfbe7a0eb2d690a33ce137
yanjinbin <yanjinbin@gmail.com>
2017-05-17 15:34:30 +0800

    Future Task 使用举例

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/excutor/callable_future/CallableTest1.java
@@ -0,0 +1,38 @@
+package com.yanjinbin.concurrent.excutor.callable_future;
+
+import java.util.concurrent.*;
+
+/**
+ * 需要确认 是 FutureTask 真正的执行类 自行了 run()方法
+ * http://www.cnblogs.com/skywang12345/p/3544116.html
+ */
+class MyCallable implements Callable {
+
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        // 执行任务
+        for (int i = 0; i < 100; i++)
+            sum += i;
+        //return sum; 
+        return Integer.valueOf(sum);
+    }
+}
+
+public class CallableTest1 {
+
+    public static void main(String[] args)
+            throws ExecutionException, InterruptedException {
+        //创建一个线程池
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        //创建有返回值的任务
+        Callable c1 = new MyCallable();
+        //执行任务并获取Future对象
+        // 实际返回的 FutureTask
+        Future f1 = pool.submit(c1);
+        // 输出结果
+        System.out.println(f1.get());
+        //关闭线程池 
+        pool.shutdown();
+    }
+}
