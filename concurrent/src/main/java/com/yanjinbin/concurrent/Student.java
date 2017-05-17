2ec03884068e382aef4d0aaba9622d7181cc7d29
yanjinbin <yanjinbin@gmail.com>
2017-05-17 15:04:43 +0800

    github issue opened for project lombook

--- concurrent/src/main/java/com/yanjinbin/concurrent/Student.java
+++ concurrent/src/main/java/com/yanjinbin/concurrent/Student.java
@@ -1,12 +1,13 @@
 package com.yanjinbin.concurrent;
 
 import lombok.Builder;
+import lombok.Data;
 
 
 /**
  * Created by yanjinbin on 2017/4/29.
  */
-
+@Data
 @Builder
 //@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Student {
