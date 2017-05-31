500a51596e6c861007782abacec7eb227ec2efb8
yanjinbin <yanjinbin@gmail.com>
2017-05-31 15:14:38 +0800

    字节码操纵技术

---
+++ Spring/src/main/java/com/yanjinbin/spring/NativeMethodAccessorImplTry.java
@@ -0,0 +1,12 @@
+package com.yanjinbin.spring;
+
+/**
+ * http://www.cnblogs.com/onlywujun/p/3519037.html
+ * 这篇文章很好的解释了字节码生成技术策略
+ *
+ * @Author Silver bullet
+ * @Since 2017/5/24.
+ */
+public class NativeMethodAccessorImplTry {
+
+}
