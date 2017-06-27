0b4b93cab2e2cf2cef6fed9d9f92226244f042bd
yanjinbin <yanjinbin@gmail.com>
2017-06-27 22:53:02 +0800

    用接口模拟可伸缩的枚举

---
+++ effectiveJava/src/main/java/chapter6/ExtendedOperation.java
@@ -0,0 +1,31 @@
+package chapter6;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/27.
+ */
+public enum ExtendedOperation implements Operations {
+
+    EXP("^") {
+        public double apply(double x, double y) {
+            return Math.pow(x, y);
+        }
+    },
+    REMAINDER("%") {
+        public double apply(double x, double y) {
+            return x % y;
+        }
+    };
+
+    private final String symbol;
+
+    ExtendedOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.symbol;
+    }
+}
---
+++ effectiveJava/src/main/java/chapter6/Operations.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/27.
+ */
+public interface Operations {
+    double apply(double x, double y);
+}
