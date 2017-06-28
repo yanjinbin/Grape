3dea15a51b51dcc2ab811940e59e9864425ce375
yanjinbin <yanjinbin@gmail.com>
2017-06-28 11:43:19 +0800

    toString 方法

---
+++ effectiveJava/src/main/java/chapter3/Foo.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/25.
+ */
+@ToString
+@EqualsAndHashCode
+public class Foo {
+    private String name;
+    private String gender;
+    private String species;
+}
