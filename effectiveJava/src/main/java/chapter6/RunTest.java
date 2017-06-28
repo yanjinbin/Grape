68a4a2358cd08154786b8fb9563c5ce6614f5965
yanjinbin <yanjinbin@gmail.com>
2017-06-28 11:42:18 +0800

    注解由于命名模式

---
+++ effectiveJava/src/main/java/chapter6/RunTest.java
@@ -0,0 +1,32 @@
+package chapter6;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/27.
+ */
+public class RunTest {
+    public static void main(String[] args) {
+        int tests = 0;
+        int passed = 0;
+        Herb herb = new Herb(Herb.Type.ANNUAL.name(),Herb.Type.ANNUAL);
+        Method[] declaredMethods = herb.getClass().getDeclaredMethods();
+        for (Method method :
+
+                declaredMethods) {
+            if (method.isAnnotationPresent(Override.class)) {
+                try {
+                    Object invoke = method.invoke(herb, herb.type);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+}
