3a376ba4e3a4052c3c2c72c735c983aa9c24315d
yanjinbin <yanjinbin@gmail.com>
2017-06-28 13:22:02 +0800

    StringBuilder

---
+++ effectiveJava/src/main/java/chapter8/StringUsage.java
@@ -0,0 +1,15 @@
+package chapter8;
+
+/**
+ * String 类型 不适合的用处
+ * 常错误用来替代枚举和基本数据类型
+ * P198
+ *
+ * @Author Silver bullet
+ * @Since 2017/6/28.
+ */
+public class StringUsage {
+    public static void main(String[] args) {
+        // ++ StringBuilder StringBuffer
+    }
+}
