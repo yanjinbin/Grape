e36abda16e549ee477e0079a7ae82815089820bd
yanjinbin <yanjinbin@gmail.com>
2017-06-29 14:20:27 +0800

    ---  无状态对象

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/chapter_2/StatelessFactorizer.java
@@ -0,0 +1,36 @@
+package com.yanjinbin.concurrent.chapter_2;
+
+import javax.annotation.concurrent.ThreadSafe;
+import javax.servlet.GenericServlet;
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.math.BigInteger;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/29.
+ */
+
+
+@ThreadSafe
+public class StatelessFactorizer extends GenericServlet implements Servlet {
+
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = factor(i);
+        encodeIntoResponse(resp, factors);
+    }
+
+    void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+    }
+
+    BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");
+    }
+
+    BigInteger[] factor(BigInteger i) {
+        // Doesn't really factor
+        return new BigInteger[] { i };
+    }
+}
