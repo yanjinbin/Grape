5e51f189050e3beb4e34905606c3535df6e784a1
yanjinbin <yanjinbin@gmail.com>
2017-07-01 11:10:47 +0800

    inmutable Object

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/chapter_3/OneValueCache.java
@@ -0,0 +1,28 @@
+package com.yanjinbin.concurrent.chapter_3;
+
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/6/30.
+ */
+public class OneValueCache {
+    private final BigInteger lastNumber;
+    private final BigInteger[] lastFactors;
+
+
+    // Arrays.copyOf() 什么用
+
+    public OneValueCache(BigInteger i, BigInteger[] factors) {
+        lastFactors = Arrays.copyOf(factors, factors.length);
+        lastNumber = i;
+    }
+
+    public BigInteger[] getFactors(BigInteger i) {
+        if (lastNumber == null || !lastNumber.equals(i)) {
+            return null;
+        } else return Arrays.copyOf(lastFactors, lastFactors.length);
+    }
+}
