42001564d070970c8ab2bd2b4e4fc37e3c6ca259
yanjinbin <yanjinbin@gmail.com>
2017-07-03 20:55:07 +0800

    ---- 重入锁

---
+++ concurrent/src/main/java/com/yanjinbin/concurrent/chapter_2/Widget.java
@@ -0,0 +1,11 @@
+package com.yanjinbin.concurrent.chapter_2;
+
+/**
+ * @Author Silver bullet
+ * @Since 2017/7/3.
+ */
+public class Widget {
+    public synchronized void doSomething(){
+        System.out.println("widget is doing");
+    }
+}
