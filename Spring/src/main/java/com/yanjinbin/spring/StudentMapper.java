0090fae25df5dc0c0f4bc71f2d9802c96a9eeddb
yanjinbin <yanjinbin@gmail.com>
2017-07-13 15:12:13 +0800

    --- mysql 在AWS上的LINUX EC2  ，数据库表名默认是区分大小写的

--- Spring/src/main/java/com/yanjinbin/spring/StudentMapper.java
+++ Spring/src/main/java/com/yanjinbin/spring/StudentMapper.java
@@ -11,14 +11,14 @@ import java.util.Date;
 @Mapper
 public interface StudentMapper {
 
-    @Select("SELECT * FROM Student WHERE NAME = #{name}")
+    @Select("SELECT * FROM student WHERE NAME = #{name}")
     Student findByName(@Param("name") String name);
 
     @Insert("INSERT INTO " +
-            "Student (name, age,gmt_create,gmt_modified) " +
+            "student (name, age,gmt_create,gmt_modified) " +
             "VALUES " +
             "(#{name}, #{age},#{gmt_create},#{gmt_modified})")
-    int insert(@Param("name") String name, @Param("age") Integer age,@Param("gmt_create") Date gmt_Create, @Param("gmt_modified") Date gmt_modified);
+    int insert(@Param("name") String name, @Param("age") Integer age, @Param("gmt_create") Date gmt_Create, @Param("gmt_modified") Date gmt_modified);
 
     @Delete("delete from student where name = #{name}")
     int deleteByName(String name);
