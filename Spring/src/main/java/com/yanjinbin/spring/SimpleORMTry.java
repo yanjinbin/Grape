0b11097aad2bc8e8cc787589fadbf94c61ad927b
yanjinbin <yanjinbin@gmail.com>
2017-07-14 12:18:53 +0800

    ---RDBMSOperation 完成基本的ORM关系映射

---
+++ Spring/src/main/java/com/yanjinbin/spring/SimpleORMTry.java
@@ -0,0 +1,30 @@
+package com.yanjinbin.spring;
+
+import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import java.util.List;
+
+/**
+ * @Author Grape
+ * @Since 2017/7/14.
+ */
+public class SimpleORMTry {
+    public static void main(String[] args) {
+        try {
+            System.out.println(getStudent(11));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Student getStudent(Integer id) throws NotFoundException {
+        CustomerMappingQuery customerMappingQuery = new CustomerMappingQuery(null);
+        Object[] params = new Object[1];
+        params[0] = id;
+        List rs = customerMappingQuery.execute(params);
+        if (rs.size() > 0) {
+            return ((Student) rs.get(0));
+        } else throw new NotFoundException("差无结果");
+    }
+}
