d89df843e2f7f6c6195dacf458776cce2fe5206d
xiaobin <yanjinbin@qq.com>
2018-11-09 12:10:00 +0800

    add timeStamp field

--- frastructure/src/main/java/com/yanjinbin/frastructue/Result.java
+++ frastructure/src/main/java/com/yanjinbin/frastructue/Result.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 /**
  * value is the return element
  * support Pagination and ext content
+ *
  * @param <Value>
  */
 public class Result<Value> implements Serializable {
@@ -44,6 +46,10 @@ public class Result<Value> implements Serializable {
     @Getter
     boolean has_more;
 
+    @Getter
+    @Setter
+    private LocalDateTime timeStamp;
+
     private Result(Value v, String msg) {
         this(v, msg, SUCCESS);
     }
@@ -53,40 +59,46 @@ public class Result<Value> implements Serializable {
         this.msg = msg;
         this.value = v;
         this.rc = rc;
-        this.has_more =true;
+        this.has_more = true;
+        this.timeStamp = LocalDateTime.now();
     }
 
-    public Result( Value v,  int rc,String msg) {
+    public Result(Value v, int rc, String msg) {
         this.msg = msg;
         this.value = v;
         this.rc = rc;
+        this.timeStamp = LocalDateTime.now();
     }
 
-    public Result( Value v, String msg, int rc,boolean has_more) {
+    public Result(Value v, String msg, int rc, boolean has_more) {
         this.msg = msg;
         this.value = v;
         this.rc = rc;
         this.has_more = has_more;
+        this.timeStamp = LocalDateTime.now();
     }
 
-    public Result(Value v,String msg,int rc ,Map exts){
-        this.value =v;
+    public Result(Value v, String msg, int rc, Map exts) {
+        this.value = v;
         this.msg = msg;
-        this.rc =rc;
+        this.rc = rc;
         this.ext = exts;
-        this.has_more =true;
+        this.has_more = true;
+        this.timeStamp = LocalDateTime.now();
     }
 
-    public Result(String msg,int rc ,Map ext){
+    public Result(String msg, int rc, Map ext) {
         this.msg = msg;
-        this.rc =rc;
+        this.rc = rc;
         this.ext = ext;
-        this.has_more =true;
+        this.has_more = true;
+        this.timeStamp = LocalDateTime.now();
     }
 
     public Result(String msg, int rc) {
         this.msg = msg;
         this.rc = rc;
+        this.timeStamp = LocalDateTime.now();
     }
 
     public static <T> Result<T> Success(T v, int rc) {
@@ -94,18 +106,18 @@ public class Result<Value> implements Serializable {
     }
 
     public static <T> Result<T> Success(T v) {
-        return new Result(v,  SUCCESS,"success");
+        return new Result(v, SUCCESS, "success");
     }
 
     public static <T> Result<T> Success(T v, String msg) {
         return new Result<T>(v, msg, SUCCESS);
     }
 
-    public static <T> Result<T> Success(T  t, String msg, int rc){
-        return new Result<>(t,msg,rc);
+    public static <T> Result<T> Success(T t, String msg, int rc) {
+        return new Result<>(t, msg, rc);
     }
 
-    public static <T> Result<T> Error(String msg,int rc){
+    public static <T> Result<T> Error(String msg, int rc) {
         return new Result<T>(msg, rc);
     }
 
@@ -113,20 +125,20 @@ public class Result<Value> implements Serializable {
         return new Result<T>(msg, FAILED);
     }
 
-    public static <T> Result<T> SuccessExt(T  t, String msg, int rc , Map ext){
-        return new Result<>(t,msg,rc,ext);
+    public static <T> Result<T> SuccessExt(T t, String msg, int rc, Map ext) {
+        return new Result<>(t, msg, rc, ext);
     }
 
-    public static <T> Result<T> SuccessExt(String msg, int rc , Map ext){
-        return new Result<>(msg,rc,ext);
+    public static <T> Result<T> SuccessExt(String msg, int rc, Map ext) {
+        return new Result<>(msg, rc, ext);
     }
 
 
-    public static <T> Result<T> PAGE(int total_record, int page_no, List<T> data){
+    public static <T> Result<T> PAGE(int total_record, int page_no, List<T> data) {
 
         PageResult<T> pageResult = new PageResult<>(total_record, page_no, data);
 
-        return  new Result(pageResult,"success",SUCCESS);
+        return new Result(pageResult, "success", SUCCESS);
     }
 
     @Data
@@ -138,8 +150,8 @@ public class Result<Value> implements Serializable {
         //页码
         private int page_no;
 
-        public PageResult(int total_records,int page_no, List<T> data) {
-            this.total_record= total_records;
+        public PageResult(int total_records, int page_no, List<T> data) {
+            this.total_record = total_records;
             this.data = data;
             this.page_no = page_no;
         }
